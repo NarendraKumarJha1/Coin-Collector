ab3321f52e7c32eea861c341ca164103690153bb
narendra kumar jha <56018997+NarendraKumarJha1@users.noreply.github.com>
2022-02-01 21:37:13 +0530

    player setup complete

---
+++ Assets/Scripts/CoinCollection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class CoinCollection : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI CoinScore;
+
+    [SerializeField]
+    AudioClip coinSFX;
+
+    [SerializeField]
+    AudioClip potionSFX;
+
+    int CoinCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoinScore.text = "XX";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Potion"))
+        {
+            AudioSource.PlayClipAtPoint(potionSFX, transform.localPosition);
+            CoinCount += 5;
+            CoinScore.text = CoinCount.ToString();
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject, 1f);
+        }
+        else if (other.gameObject.CompareTag("Coin"))
+        {
+            AudioSource.PlayClipAtPoint(coinSFX, transform.localPosition);
+            CoinCount += 1;
+            CoinScore.text = CoinCount.ToString();
+            other.gameObject.SetActive(false);
+            Destroy(other.gameObject, 1f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/SkyboxHandler.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxHandler : MonoBehaviour
+{
+    public Material[] SkyBoxArray;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        skyBoxHandler();
+
+    }
+    void skyBoxHandler()
+    {
+        //    int skyBoxCount = 0;
+        //    if (Input.GetKey("UpArrow"))
+        //    {
+        //        RenderSettings.skybox = SkyBoxArray[skyBoxCount];
+        //        skyBoxCount += 1;
+        //        if (skyBoxCount >= 11)
+        //        {
+        //            skyBoxCount = 0;
+        //        }
+        //    }
+
+
+        if (Input.GetButtonDown("1"))
+        {
+            RenderSettings.skybox = SkyBoxArray[0];
+        }
+        else if (Input.GetButtonDown("2"))
+        {
+            RenderSettings.skybox = SkyBoxArray[1];
+        }
+        else if (Input.GetButtonDown("3"))
+        {
+            RenderSettings.skybox = SkyBoxArray[2];
+        }
+        else if (Input.GetButtonDown("4"))
+        {
+            RenderSettings.skybox = SkyBoxArray[3];
+        }
+        else if (Input.GetButtonDown("5"))
+        {
+            RenderSettings.skybox = SkyBoxArray[4];
+        }
+        else if (Input.GetButtonDown("6"))
+        {
+            RenderSettings.skybox = SkyBoxArray[5];
+        }
+        else if (Input.GetButtonDown("7"))
+        {
+            RenderSettings.skybox = SkyBoxArray[6];
+        }
+        else if (Input.GetButtonDown("8"))
+        {
+            RenderSettings.skybox = SkyBoxArray[7];
+        }
+        else if (Input.GetButtonDown("9"))
+        {
+            RenderSettings.skybox = SkyBoxArray[8];
+        }
+        else if (Input.GetButtonDown("0"))
+        {
+            RenderSettings.skybox = SkyBoxArray[9];
+        }
+        else if (Input.GetButtonDown("a"))
+        {
+            RenderSettings.skybox = SkyBoxArray[10];
+        }
+        else if (Input.GetButtonDown("b"))
+        {
+            RenderSettings.skybox = SkyBoxArray[11];
+        }
+
+
+
+    }
+}
